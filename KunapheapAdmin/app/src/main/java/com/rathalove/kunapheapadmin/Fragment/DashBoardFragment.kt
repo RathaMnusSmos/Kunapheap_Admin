3d44c73b5e973a4c10604cb285ff321b1061bae3
RathaMnusSmos <RathaMnusSmos@gmail.com>
2023-03-16 03:15:52 +0700

    Modern Drawer with animation and modify fragment

---
+++ KunapheapAdmin/app/src/main/java/com/rathalove/kunapheapadmin/Fragment/DashBoardFragment.kt
@@ -0,0 +1,22 @@
+package com.rathalove.kunapheapadmin.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rathalove.kunapheapadmin.R
+
+class DashBoardFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var root: ViewGroup = inflater.inflate(R.layout.dashbaord_fragment, container, false) as ViewGroup
+//        return super.onCreateView(inflater, container, savedInstanceState)
+        return root
+    }
+}
